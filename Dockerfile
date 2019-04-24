e46c8c9f9c4c1de24677402559e140c17ab9a053
Chris Devrell <CD0125@altushq.local>
2019-04-24 13:53:02 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM plexinc/pms-docker:latest
+
+# Install wget dependancy
+RUN apt-get -y update
+RUN apt-get -y install wget
+
+# Add script directory
+RUN mkdir /scripts
+
+# Copy Script to enable decoding
+COPY nvdec_enable.sh /scripts/nvdec_enable.sh
+
+# Make script executable
+RUN chmod +x /scripts/nvdec_enable.sh
+
+# Execute script
+RUN /bin/sh /scripts/nvdec_enable.sh
+
+VOLUME /config /transcode
+EXPOSE 32400/tcp 3005/tcp 8324/tcp 32469/tcp 1900/udp 32410/udp 32412/udp 32413/udp 32414/udp
+
+# Example Usage
+# sudo docker build --no-cache -t plex-with-nvdec .
+# sudo docker run -d \
+#     --name plex \
+#     --restart=unless-stopped \
+#     --network=xxxxx \
+#     --ip=xxx.xxx.xxx.xxx \
+#     -v plex_db:/config \
+#     -v plex_logs:"/config/Library/Application Support/Plex Media Server/Logs" \
+#     -v /data/ssd/plex_transcode:/transcode \
+#     -v /data/nas/media/TV:/data/TV \
+#     -v /data/nas/media/Movies:/data/Movies \
+#     plex-with-nvdec
---
+++ nvdec_enable.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+plex_nvdec_url="https://raw.githubusercontent.com/revr3nd/plex-nvdec/master/plex-nvdec-patch.sh"
+
+# Change the variable below if you wish to edit which codecs are decoded:
+CODECS=("h264" "hevc" "mpeg2video" "mpeg4" "vc1" "vp8" "vp9")
+
+# Turn the CODECS array into a string of arguments for the wrapper script:
+if [ "$CODECS" ]; then
+	codec_arguments=""
+	for format in "${CODECS[@]}"; do
+		codec_arguments+=" -c ${format}"
+	done
+fi
+
+echo -n "<b>Applying hardware decode patch... </b><br/>"
+
+# Grab the latest version of the plex-nvdec-patch.sh from github:
+echo 'Downloading patch script...'
+/bin/sh -c "wget -q --show-progress --progress=bar:force:noscroll -P /usr/lib/plexmediaserver/ ${plex_nvdec_url}"
+
+# Make the patch script executable.
+chmod +x "/usr/lib/plexmediaserver/plex-nvdec-patch.sh"
+
+# Run the script, with arguments for codecs, if present.
+command="/usr/lib/plexmediaserver/plex-nvdec-patch.sh"
+if [ "$codec_arguments" ]; then
+	command+="${codec_arguments}"
+	/bin/sh -c "${command}${codec_arguments}"
+else
+	/bin/sh -c "${command}"
+fi
